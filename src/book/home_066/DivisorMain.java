0dbb2a8b278718f4b321fdfb746b6a7a9b720c60
jingoonHome <parkman108@gmail.com>
2021-01-24 18:11:51 +0900

    066 약수구하기

---
+++ src/book/home_066/DivisorMain.java
@@ -0,0 +1,11 @@
+package book.home_066;
+
+public class DivisorMain {
+	
+	public static void main(String[] args) {
+		System.out.println("5가 10의 약수?"+ Divisor.isMod(10, 5));
+		System.out.println("5가 13의 약수?"+ Divisor.isMod(13, 5));
+		Divisor.printDivisor(15);//15의 모든 약수 1포함
+	}
+
+}
