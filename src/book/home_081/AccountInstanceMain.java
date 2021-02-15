798eb8e6793226b338c694eab6aeb4e5b8100b6c
jingoonHome <parkman108@gmail.com>
2021-02-15 18:38:22 +0900

    추상화와 클래스화, 특징과 목적의 표현-설계도

---
+++ src/book/home_081/AccountInstanceMain.java
@@ -0,0 +1,13 @@
+package book.home_081;
+
+public class AccountInstanceMain {
+
+	public static void main(String[] args) {
+		Account acc1 = new Account();
+		Account acc2 = new Account(1000);
+		Account acc3 = new Account(1000);
+	
+		System.out.println(acc2==acc3);	//false
+		System.out.println(acc3.equals(acc3));	//true
+	}
+}
