8d55d54293ee201386362a4c5141e63c250415ad
jingoon <parkman108@gmail.com>
2021-04-12 19:29:00 +0900

    상속 Ant

---
+++ src/book/home_098/WaterAnt.java
@@ -0,0 +1,14 @@
+package book.home_098;
+
+public class WaterAnt extends Ant{
+
+	private String where ="water";
+	
+	public void show() {
+		System.out.println(where+"주변");
+	}
+	
+	public void print() {
+		System.out.println(getName()+"은 "+where+"에 산다." );
+	}
+}
